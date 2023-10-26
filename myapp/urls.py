41617bdf649e391c111979d28859183d062dd43c
Hamilton <hamilt727@gmail.com>
2023-10-26 17:30:25 -0500

    se realiza el crud de los contactos, a falta del buscado por nombre y numero

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index)
+    path('',views.index,name='inicio'),
+    path('list/',views.list,name='lista'),
+    path('add/',views.addContact,name='agregar'),
+    path('edit/<int:id>',views.editContact,name='editar'),
+    path('delete/<int:id>/',views.deleteContact,name='eliminar'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .models import Contact
+from .form import ContactForm
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
 
-def hello(request):
-    return HttpResponse("<h1>HOLA MUNDO</h1>")
+
+def list(request):
+    contacts = Contact.objects.all()
+    return render(request,'contacts/list.html',{'contacts':contacts})
+
+
+def addContact(request):
+    formulario = ContactForm(request.POST or None,request.FILES or 
+    None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('lista')
+    return render(request,'contacts/add.html',{'formulario':formulario})
+
+
+def editContact(request,id):
+    contact = Contact.objects.get(id=id)
+    formulario = ContactForm(request.POST or None,request.FILES or None ,instance=contact)
+    if formulario.is_valid() and request.method == 'POST':
+        formulario.save()
+        return redirect('lista')
+    return render(request,'contacts/update.html',{'formulario':formulario})
+
+def deleteContact(request,id):
+    contact = Contact.objects.get(id=id) 
+    contact.delete()
+    return redirect('lista')
+
+
 
